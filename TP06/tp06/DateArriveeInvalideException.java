8a46d1249396e22e40609bd4cdd66b7c5605d72e
Frodon001 <55687537+Frodon001@users.noreply.github.com>
2019-11-17 14:37:54 +0100

    TP06 15 novembre 2019

---
+++ TP06/tp06/DateArriveeInvalideException.java
@@ -0,0 +1,12 @@
+package tp06;
+
+public class DateArriveeInvalideException extends Exception {
+	public DateArriveeInvalideException() {
+		super();
+	}
+
+	public DateArriveeInvalideException(String message) {
+		super(message);
+	}
+
+}
